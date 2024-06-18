40b83fe7ca600fa8ca8a2d527459fdafab57ca0a
mjcastroUS <marcasper@alum.us.es>
2024-06-18 16:25:56 +0200

    fix api

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ from langchain.llms import OpenAI
 
 st.title('🦜🔗 Quickstart App')
 
-#openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
-openai_api_key = "sk-proj-SEY6o1ZbHo8nqsmV3pCHT3BlbkFJXWdCqRWR4FiKxTlk9dUj"
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+
 
 def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
